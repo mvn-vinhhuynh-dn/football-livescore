d587b44b6d26c363dd7f7c9d3c07e8fc8dce884f
xuanphuktvn1 <xuanphuktvn1@gmail.com>
2015-11-19 13:53:10 +0700

    Merge branch 'master' of https://bitbucket.org/victorydst3/football-livescore into new-xuanphu-20151109-add_video
    Conflicts:
    	app/build.gradle

--- app/src/main/java/com/androidbelieve/drawerwithswipetabs/interfaces/ItemStickyClick.java
+++
@@ -1,9 +0,0 @@
-package com.androidbelieve.drawerwithswipetabs.interfaces;
-
-/**
- * Copyright © 2015 AsianTech inc.
- * Created by VinhHlb on 10/29/15.
- */
-public interface ItemStickyClick {
-    void onItemStickyClick(String homeName, String difName, String url);
-}
--- app/src/main/java/com/androidbelieve/drawerwithswipetabs/interfaces/OnEditListener.java
+++
@@ -1,8 +0,0 @@
-package com.androidbelieve.drawerwithswipetabs.interfaces;
-
-/**
- * Created by Robby on 1/27/2015.
- */
-public interface OnEditListener {
-     void onEdit(int position);
-}
