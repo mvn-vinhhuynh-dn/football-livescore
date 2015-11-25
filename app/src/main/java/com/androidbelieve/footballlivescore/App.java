a50243d7d80f2414d99bf368b49666dd10d96fca
Huynh Le Ba Vinh <vinhhlb@asiantech.vn>
2015-11-25 14:34:10 +0700

    add google analytics

--- app/src/main/java/com/androidbelieve/footballlivescore/App.java
+++ app/src/main/java/com/androidbelieve/footballlivescore/App.java
@@ -37,7 +37,7 @@ public class App extends Application {
         if (mTracker == null) {
             GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
             // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
-            mTracker = analytics.newTracker(R.xml.app_tracker);
+            mTracker = analytics.newTracker(R.xml.global_tracker);
         }
         return mTracker;
     }
